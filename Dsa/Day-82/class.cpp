e967c80891cfb2d72b5bfeca2cf22f485dcc2bdd
Namo manikanta Sai <manikantanamo@gmail.com>
2023-12-18 17:18:49 +0530

    Day-82

---
+++ Dsa/Day-82/class.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void merge(int arr[],int start,int mid,int end)
+{
+    vector<int>temp(end-start+1);
+    int left = start,right = mid + 1,index = 0;
+
+    while(left <= mid && right <= end)
+    {
+        if(arr[left] <= arr[right])
+        {
+            temp[index] = arr[left];
+            index++,left++;
+        }
+        else
+        {
+            temp[index] = arr[right];
+            index++,right++;
+        }
+    }
+
+    // left is not yet empty
+    while(left <= mid)
+    {
+        temp[index] = arr[left];
+        index++,left++;
+    }
+    // right is not yet empty
+    while(right<= end)
+    {
+        temp[index] = arr[right];
+        index++,right++;
+    }
+    index = 0;
+    while(start<=end)
+    {
+        arr[start] = temp[index];
+        start++,index++;
+    }
+}
+
+
+void mergesort(int arr[],int start,int end)
+{
+    // base condition
+    if(start == end)
+     return;
+    int mid = start + (end - start)/2;
+    // left
+    mergesort(arr,start,mid);
+    //Right
+    mergesort(arr,mid + 1,end);
+    merge(arr,start,mid,end);
+}
+
+int main()
+{
+    int arr[] = {6,3,1,2,8,9,10,7,3,10};
+    mergesort(arr,0,9);
+
+    for(int i = 0;i < 10;i++)
+        cout << arr[i] << " ";
+}
